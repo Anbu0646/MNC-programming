812776bc6416cf351022e7cabe14c723bfa8d24e
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-04-25 21:40:34 +0530

    Create Sum of non-prime digits.py

---
+++ Sum of non-prime digits.py
@@ -0,0 +1,34 @@
+'''
+The program must accept an integer N as the input. The program must print the sum of non-prime digits in N as the output. 
+If there is no such digit, the program must print -1 as the output. 
+
+Condition(s): 1 <= N <= 10^8 
+
+Input Format:  The first line contains N. 
+Output Format: The first line the sum of non-prime digits in N or -1. 
+
+Example Input/Output 1:
+
+Input:  219517 
+Output: 11 
+
+Explanation: The non-prime digits in 219517 are 1, 9 and 1. 
+             The sum of non-prime digits is 11 (1 + 9 + 1).
+             So 11 is printed.
+             
+Example Input/Output 2:
+
+Input:  3005 
+Output: 0
+
+Explanation:  The non-prime digits in 3005 are 0 and 0. 
+              The sum of non-prime digits is 0. 
+              So 0 is printed. 
+              
+Example Input/Output 3:
+
+Input:  725
+Output: -1
+
+SOLUTION:
+'''
